f014e8beca4e59b260634b9f591ee9cc354e87c3
rakibul114 <rakibulislam114@gmail.com>
2022-07-06 22:57:17 +0600

    sidenav added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
+import SideNav from './components/SideNav/SideNav';
+import Skills from './components/Skills/Skills';
 
 function App() {
   return (
-    <div>
+    <div className='flex bg-black'>
+      <SideNav/>
       <Routes>
-        <Route path='/' element={<Home/> } />
+        <Route path='/' element={<Home />} />
+        <Route path='skills' element={<Skills/>} />
       </Routes>
     </div>
   );
