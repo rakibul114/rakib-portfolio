775a33f91ef9442d6060a94b779202c71077b164
rakibul114 <rakibulislam114@gmail.com>
2022-07-06 12:21:04 +0600

    created all required components and insalled tailwind and react-router

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>I am a frontend developer</h1>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/SideNav/SideNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideNav = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    );
+};
+
+export default SideNav;
